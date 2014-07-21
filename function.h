05c9e16e54bf0553e81497da67694b0ad89432e6
celvins <vard-94@mail.ru>
2014-07-21 12:45:56 +0400

    Create function.h

---
+++ function.h
@@ -0,0 +1,14 @@
+/*
+ * function.h
+ *
+ *  Created on: 18.07.2014
+ *      Author: Àíäðàíèê
+ */
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+double fun(double*);
+double* huk(double*, double*, double, double, int);
+
+#endif /* FUNCTION_H_ */
